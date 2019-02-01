c9cbcc242dcc5668017133160ea745fadb90026a
Jacob Kaufmann <jacobkaufmann18@gmail.com>
2019-01-31 21:32:55 -0600

    Adjust for new Database type

--- pkg/datastore/datastore.go
+++ pkg/datastore/datastore.go
@@ -1,26 +1,23 @@
 package datastore
 
 import (
-	"github.com/fatih/color"
 	arxivlib "github.com/jacobkaufmann/arxivlib-papers"
-	"github.com/mongodb/mongo-go-driver/mongo"
 )
 
 // A Datastore accesses the datastore (MongoDB)
 type Datastore struct {
 	Papers arxivlib.PapersService
 
-	db *mongo.Database
+	db *Database
 }
 
 // NewDatastore creates a new client for accessing the datastore
-func NewDatastore(db *mongo.Database) *Datastore {
+func NewDatastore(db *Database) *Datastore {
 	if db == nil {
-		color.Magenta("NILLL")
 		db = DB
 	}
+
 	d := &Datastore{db: db}
 	d.Papers = &papersStore{d}
-
 	return d
 }
